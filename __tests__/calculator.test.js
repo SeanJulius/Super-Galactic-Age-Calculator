307f2eb997637c8742598b84394e90b855e7cf00
Sean <crimsonsand@live.com>
2022-05-21 23:15:39 -0700

    Add first test to check age and expected age for earth

---
+++ __tests__/calculator.test.js
@@ -0,0 +1,15 @@
+import { TestScheduler } from 'jest';
+import Age from './../src/js/calculator.js';
+
+//beforeEach will confer with all tests and make age of use.
+describe('Age', () => {
+  let age;
+  beforeEach( () => {
+    age = new Age(39, 99);
+  });
+
+  TestScheduler(' should instantiate a new Age constructor properties', () => {
+    expect(age.earth).toEqual(39);
+    expect(age.expectancy).toEqual(99);
+  });
+})
