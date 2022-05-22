dd3f9e70751ee56103e903b89c4216a528e07c68
Sean <crimsonsand@live.com>
2022-05-22 00:32:46 -0700

    Add test 3 is turning over some passing results

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -5,9 +5,14 @@ export default class Age {
       this.earth = earth;
       this.expectancy = expectancy;
     }
-
+      //toFixed() will round to a whole number
     mercury() {
       this.earth = parseInt((this.earth / .24).toFixed());
+      return this.earth;
+    }
+
+    venus() {
+      this.earth = parseInt((this.earth / .62).toFixed());
       return this.earth;
     }
 }
